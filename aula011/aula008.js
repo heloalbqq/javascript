713924fbf9a16624b3a865d9940b55356bfa6e82
Heloisa Albuquerque <heloisaalbuquerque1706@gmail.com>
2024-05-20 16:08:11 -0300

    condições js

---
+++ aula011/aula008.js
@@ -0,0 +1,8 @@
+var veloc = 62
+console.log(`A velocidade do seu carro é ${veloc}Km/h`)
+
+if (veloc >= 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
