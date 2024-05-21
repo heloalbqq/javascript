7a41b8a0933fe2e2d3bd29851f82011ca12429b4
Heloisa Albuquerque <heloisaalbuquerque1706@gmail.com>
2024-05-21 15:48:39 -0300

    repetições js

---
+++ aula013/ambiente.js
@@ -0,0 +1,27 @@
+/*
+ ENQUANTO
+
+ console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+
+var contador = 1
+
+while (contador <= 6) {
+    console.log('tudo bem?')
+    contador++ //contador <- contador + 1
+}
+
+*/
+
+  //FACA ENQUANTO
+
+  var contador = 1
+
+  do {
+    console.log(`Passo ${contador}`)
+    contador++
+  } while (contador <= 6)
---
+++ aula014/ambiente.js
@@ -0,0 +1,23 @@
+
+/*
+   var contador = 1
+
+   while(contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+   }
+
+   >>>>>>>>PARA<<<<<<<<
+
+   for(var contador = 1; contador <= 6; contador++) {
+    console.log(`Passo ${contador}`)
+}
+*/
+
+//MODO DE DEPURAÇÃO
+
+console.log('vai começar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM')
