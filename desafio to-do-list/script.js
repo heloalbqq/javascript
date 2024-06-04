3daef0969eb9fc013344137982e40cf2d317eeb4
Heloisa Albuquerque <heloisaalbuquerque1706@gmail.com>
2024-06-04 20:22:34 -0300

    terminei desafio to do

--- desafio to-do-list/script.js
+++ desafio to-do-list/script.js
@@ -7,6 +7,8 @@
     const editInput =  document.querySelector("#edit-input")
     const cancelEditBtn =  document.querySelector("#cancel-edit-btn")
 
+    let oldInputValue
+
 //  Funções
 
     const saveTodo = (text) => {
@@ -24,7 +26,7 @@
 
         const editBtn = document.createElement('button')
         editBtn.classList.add('edit-todo')
-        editBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+        editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
         todo.appendChild(editBtn)
 
         const deleteBtn = document.createElement('button')
@@ -36,6 +38,25 @@
 
         todoInput.value = ''
         todoInput.focus()
+    }
+
+    const toggleForms = () => {
+        //qnd clicar em editar, a tela fica limpa
+        editForm.classList.toggle('hide') 
+        todoForm.classList.toggle('hide')
+        todoList.classList.toggle('hide')
+    }
+     //envia o texto editado
+    const updateTodo = (text) => {
+        const todos = document.querySelectorAll('.todo')
+
+        todos.forEach((todo) => { //olha todas as tarefas
+            let todoTitle = todo.querySelector('h3') //lê as tarefas
+
+            if(todoTitle.innerText === oldInputValue) {
+                todoTitle.innerText = text //edita a tarefa
+            }
+        })
     }
 //  Eventos
 
@@ -51,19 +72,43 @@
         }
     })
 
-        document.addEventListener('click', (e) => {
-            const targetEl = e.target //p saber onde clicou
-            const parentEl = targetEl.closest('div') //para achar a "caixa" de onde vc clicou
+    document.addEventListener('click', (e) => {
+        const targetEl = e.target //p saber onde clicou
+        const parentEl = targetEl.closest('div') //para achar a "caixa" de onde vc clicou
+        let todoTitle
 
-            if(targetEl.classList.contains('finish-todo')) { //verif se tem a var
-                parentEl.classList.toggle('done') //marca como feito
-            }
+        if(parentEl && parentEl.querySelector('h3')) {
+            todoTitle = parentEl.querySelector('h3').innerText
+        }
 
-            if(targetEl.classList.contains('remove-todo')) {
-                parentEl.remove()
-            }
+        if(targetEl.classList.contains('finish-todo')) { //verif se tem a var
+            parentEl.classList.toggle('done') //marca como feito
+        }
 
-            if(targetEl.classList.contains('edit-todo')) {
-                console.log('editou')
-            }
-        })
+        if(targetEl.classList.contains('remove-todo')) {
+            parentEl.remove()
+        }
+
+        if(targetEl.classList.contains('edit-todo')) {
+            toggleForms()
+            editInput.value = todoTitle
+            oldInputValue = todoTitle //salvando no banco de dados
+        }
+    })
+
+    cancelEditBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        toggleForms()
+    })
+    
+    editForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const editInputValue = editInput.value
+
+        if(editInputValue) {
+            //atualizar a tarefa
+            updateTodo(editInputValue)
+        }
+
+        toggleForms()
+    })
